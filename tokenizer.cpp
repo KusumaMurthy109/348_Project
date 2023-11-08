41488218c8e5162e03a9a570f9d067e2f4beded9
Reeny <ywreenyhuang@gmail.com>
2023-11-08 17:52:11 -0600

    pushing the main file and the tokenizer file

---
+++ tokenizer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include "main.cpp"
+const int MAX = 1000;
+
+class Tokenizer {
+    public:
+        int num;
+        double doublenum;
+        char equation_array[MAX];
+        int equation_converter[MAX];
+
+    void hello(){
+        
+    }
+
+};

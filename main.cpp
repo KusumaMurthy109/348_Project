7995848c8e769c46e0608e5320f40905cf1257e5
KusumaSM10 <kusuma.s.murthy@ku.edu>
2023-11-13 17:00:56 -0600

    tokenizer works, but not correct for space and '-'

--- main.cpp
+++ main.cpp
@@ -14,3 +14,14 @@ int main(){
 
     return 0;
 }
+
+/*
+TO RUN THE CODE:
+Type into you terminal:
+    g++ -c main.cpp -o main.o
+    g++ -c tokenizer.cpp -o tokenizer.o
+    g++ main.o tokenizer.o -o myprogram
+
+and then enter the following command into the terminal:
+    ./myprogram
+*/
---
+++ tokenizer.h
@@ -0,0 +1,11 @@
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void tokenizer_input(const string& UserInput);
+
+#endif

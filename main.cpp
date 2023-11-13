b520a2c7ba8527123a12faa34b75a6565c466f33
KusumaSM10 <kusuma.s.murthy@ku.edu>
2023-11-13 17:18:09 -0600

    works, but not correctly for spaces and '-'

---
+++ main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include "tokens.h"
+
+using namespace std;
+
+int main(){
+    string UserInput;
+
+    cout << "Enter you expression: ";
+    cin >> UserInput;
+
+    tokenizer_input(UserInput);
+
+    return 0;
+}
+
+/*
+TO RUN THE CODE:
+Type into you terminal:
+    g++ -c main.cpp -o main.o
+    g++ -c tokens.cpp -o tokens.o
+    g++ main.o tokens.o -o myprogram
+
+and then enter the following command into the terminal:
+    ./myprogram
+*/
---
+++ tokens.h
@@ -0,0 +1,13 @@
+//tokenizer template
+
+#ifndef TOKENIZER_H
+#define TOKENIZER_H
+
+#include <vector>
+#include <string>
+#include <sstream>
+using namespace std;
+
+void tokenizer_input(const string& UserInput);
+
+#endif
